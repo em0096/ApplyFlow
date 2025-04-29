32fa45a6d830e0888ef182d22ca229eca4bfcf6a
em315 <em315@LAB-RG11-12>
2025-04-29 18:29:41 +1200

    Add project files.

---
+++ ApplyFlow/Form1.cs
@@ -0,0 +1,38 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace ApplyFlow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string connectionString = ConfigurationManager.ConnectionStrings["MyOracleDB"].ConnectionString;
+            OracleConnection connection = new OracleConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Connected to Oracle Database!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
