4c4fb4871299d5f349525406ba0f38d8ede1859d
jl846 <jl846@students.waikato.ac.nz>
2025-05-26 13:44:47 +1200

    Home screen Info

--- ApplyFlow/Home.Designer.cs
+++ ApplyFlow/Home.Designer.cs
@@ -30,6 +30,12 @@
         {
             this.dataGridApplications = new System.Windows.Forms.DataGridView();
             this.buttonNewApplication = new System.Windows.Forms.Button();
+            this.User = new System.Windows.Forms.Label();
+            this.Avg = new System.Windows.Forms.Label();
+            this.Jobs = new System.Windows.Forms.Label();
+            this.SPD = new System.Windows.Forms.Label();
+            this.listBoxCountry = new System.Windows.Forms.ListBox();
+            this.listBoxInterviews = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridApplications)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,10 +44,11 @@
             this.dataGridApplications.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.dataGridApplications.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridApplications.GridColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.dataGridApplications.Location = new System.Drawing.Point(43, 92);
+            this.dataGridApplications.Location = new System.Drawing.Point(617, 92);
             this.dataGridApplications.Name = "dataGridApplications";
-            this.dataGridApplications.Size = new System.Drawing.Size(1046, 427);
+            this.dataGridApplications.Size = new System.Drawing.Size(472, 427);
             this.dataGridApplications.TabIndex = 0;
+            this.dataGridApplications.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridApplications_CellContentClick);
             // 
             // buttonNewApplication
             // 
@@ -54,18 +61,81 @@
             this.buttonNewApplication.UseVisualStyleBackColor = true;
             this.buttonNewApplication.Click += new System.EventHandler(this.buttonNewApplication_Click);
             // 
+            // User
+            // 
+            this.User.AutoSize = true;
+            this.User.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.User.Location = new System.Drawing.Point(12, 9);
+            this.User.Name = "User";
+            this.User.Size = new System.Drawing.Size(85, 20);
+            this.User.TabIndex = 2;
+            this.User.Text = "UserName";
+            // 
+            // Avg
+            // 
+            this.Avg.AutoSize = true;
+            this.Avg.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Avg.Location = new System.Drawing.Point(33, 49);
+            this.Avg.Name = "Avg";
+            this.Avg.Size = new System.Drawing.Size(158, 25);
+            this.Avg.TabIndex = 3;
+            this.Avg.Text = " :Average Salary";
+            // 
+            // Jobs
+            // 
+            this.Jobs.AutoSize = true;
+            this.Jobs.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Jobs.Location = new System.Drawing.Point(31, 92);
+            this.Jobs.Name = "Jobs";
+            this.Jobs.Size = new System.Drawing.Size(66, 25);
+            this.Jobs.TabIndex = 4;
+            this.Jobs.Text = " :Jobs";
+            // 
+            // SPD
+            // 
+            this.SPD.AutoSize = true;
+            this.SPD.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SPD.Location = new System.Drawing.Point(36, 135);
+            this.SPD.Name = "SPD";
+            this.SPD.Size = new System.Drawing.Size(246, 25);
+            this.SPD.TabIndex = 5;
+            this.SPD.Text = "Sucess, Pending, Declined";
+            // 
+            // listBoxCountry
+            // 
+            this.listBoxCountry.FormattingEnabled = true;
+            this.listBoxCountry.Location = new System.Drawing.Point(16, 253);
+            this.listBoxCountry.Name = "listBoxCountry";
+            this.listBoxCountry.Size = new System.Drawing.Size(214, 108);
+            this.listBoxCountry.TabIndex = 6;
+            // 
+            // listBoxInterviews
+            // 
+            this.listBoxInterviews.FormattingEnabled = true;
+            this.listBoxInterviews.Location = new System.Drawing.Point(16, 372);
+            this.listBoxInterviews.Name = "listBoxInterviews";
+            this.listBoxInterviews.Size = new System.Drawing.Size(575, 147);
+            this.listBoxInterviews.TabIndex = 7;
+            // 
             // FormHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1123, 558);
+            this.Controls.Add(this.listBoxInterviews);
+            this.Controls.Add(this.listBoxCountry);
+            this.Controls.Add(this.SPD);
+            this.Controls.Add(this.Jobs);
+            this.Controls.Add(this.Avg);
+            this.Controls.Add(this.User);
             this.Controls.Add(this.buttonNewApplication);
             this.Controls.Add(this.dataGridApplications);
             this.Name = "FormHome";
             this.Text = "ApplyFlow";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridApplications)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,5 +143,11 @@
 
         private System.Windows.Forms.DataGridView dataGridApplications;
         private System.Windows.Forms.Button buttonNewApplication;
+        private System.Windows.Forms.Label User;
+        private System.Windows.Forms.Label Avg;
+        private System.Windows.Forms.Label Jobs;
+        private System.Windows.Forms.Label SPD;
+        private System.Windows.Forms.ListBox listBoxCountry;
+        private System.Windows.Forms.ListBox listBoxInterviews;
     }
 }
